7dc765a226bdd3fd1e4191721e936169dbdeced6
m0r13 <moritz.hilscher@gmail.com>
2017-12-14 03:38:53 +0200

    Add pyro example

---
+++ pyrotest/client.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+
+import random
+import Pyro4
+
+
+# We need to set either a socket communication timeout,
+# or use the select based server. Otherwise the daemon requestLoop
+# will block indefinitely and is never able to evaluate the loopCondition.
+Pyro4.config.COMMTIMEOUT = 0.5
+
+class SessionHandler(object):
+
+    @Pyro4.expose
+    def sessionStarted(self):
+        print("handler: sessionStarted")
+
+    @Pyro4.expose
+    def sudokuChanged(self, sudoku):
+        pass
+
+    @Pyro4.expose
+    def scoresChanged(self, scores):
+        pass
+
+    @Pyro4.expose
+    def gameOver(self, winner):
+        print("handler: gameOver(winner='%s')" % winner)
+
+with Pyro4.core.Daemon() as daemon:
+    # register our callback handler
+    callback = SessionHandler()
+    daemon.register(callback)
+
+    # contact the server and put it to work
+    print("creating a bunch of workers")
+    with Pyro4.core.Proxy("PYRO:sudoku@localhost:9999") as server:
+        print("Got server: %s" % server)
+        # can do smth like
+        # server.listSessions()
+        # server.createSession()
+        # server.provideSessionCallback()
+        # etc.
+        pass
+
+        print("Running sessions: %s" % server.listSessions())
+        print("Joining session 0: %s" % server.joinSession(0))
+        server.provideSessionCallback(callback)
+
+    print("Waiting on callback...")
+    daemon.requestLoop()
+    #daemon.requestLoop(loopCondition=lambda: CallbackHandler.workdone < NUM_WORKERS)
---
+++ pyrotest/server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+import time
+import threading
+import Pyro4
+
+def get_id():
+    peer = Pyro4.current_context.client_sock_addr
+    ip = Pyro4.socketutil.getIpAddress(peer[0], workaround127=True)
+    return "%s:%d" % (ip, peer[1])
+
+class SudokuServer(object):
+    def __init__(self):
+        pass
+
+    @Pyro4.expose
+    def listSessions(self):
+        return [0, 1, 2]
+
+    @Pyro4.expose
+    def createSession(self, name, num_players):
+        pass
+
+    @Pyro4.expose
+    def joinSession(self, session):
+        return True
+
+    @Pyro4.expose
+    def provideSessionCallback(self, callback):
+        # check if he's really in session... blah
+        # register callback for server
+        # it can be used later to notify the client about session changes
+        # see client.py SessionHandler
+        
+        # smth like
+        # self.client_callbacks[get_id()] = callback
+
+        print("Got client callback: %s" % callback)
+        def answer(callback=callback):
+            callback.sessionStarted()
+            time.sleep(5)
+            callback.gameOver("You!")
+        threading.Thread(target=answer).start()
+
+    @Pyro4.expose
+    def suggestNumber(self, i, j, value):
+        pass
+
+Pyro4.Daemon.serveSimple({
+    SudokuServer: "sudoku"
+}, host="localhost", port=9999, ns=False)
