0dbade92ecec4e5367f9ae763718527f0c58eebe
Bruno Produit <bruno.produit@protonmail.ch>
2017-12-14 14:01:55 +0200

    server ported to pyro4

--- server.py
+++ server.py
@@ -1,93 +1,75 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 
-"""
-usage: server.py [-h] [-v] [-H HOST] [-p PORT]
+import time
+import threading
+import Pyro4
 
-Concurrent Sudoku
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -v, --version         show program's version number and exit
-  -H HOST, --host HOST  Server TCP port, defaults to 127.0.0.1
-  -p PORT, --port PORT  Server TCP port, defaults to 8888
-"""
-
-import time, threading, uuid, util                          # Miscellaneous utils
-from protocol import *                                      # Import our own protocol class
-from sudoku import *                                        # Import our own sudoku class
-from socket import AF_INET, SOCK_STREAM, SHUT_WR, socket    # Used for TCP networking
-from argparse import ArgumentParser                         # Parsing command line arguments
-import traceback
+def get_id():
+    peer = Pyro4.current_context.client_sock_addr
+    ip = Pyro4.socketutil.getIpAddress(peer[0], workaround127=True)
+    return "%s:%d" % (ip, peer[1])
 
 class Manager():
     """Contains the global variables for the server,
     acts as manager and is passed to all Threads"""
-    
+
     def __init__(self):
         """Manager constructor, shared variables for the threads"""
         self.ServerGames = {}
-        self.clients = []
-        self.ServerUsernames = [] 
+        self.ServerUsernames = []
+        self.client_callbacks = {}
         print "Created manager"
-    
+
     def notify_score(self, game):
         """ Notify all users in the same game about the scores"""
-        for i in self.clients:
-            if game.get_uuid() == i.game.get_uuid():
-                write_package(i.socket, PKG_SCORES_STATE, {'scores': game.get_scores()})
-                
+        for i in self.game.users.keys():
+            client_callbacks[i].scoresChanged(game.get_scores())
+
     def notify(self, game):
         """ Notify all users in the same game about the changes in the sudoku"""
-        for i in self.clients:
-            if game.get_uuid() == i.game.get_uuid():
-                write_package(i.socket, PKG_SUDOKU_STATE, \
-                              {'sudoku': game.get_sudoku().serialize()})
-                write_package(i.socket, PKG_SCORES_STATE, {'scores': game.get_scores()})
-        
+        for i in self.game.users.keys():
+            client_callbacks[i].sudokuChanged(game.get_sudoku().serialize())
+            client_callbacks[i].scoresChanged(game.get_scores())
+
     def game_over(self, game):
         """ Notify all users in the same game when game is over"""
-        for i in self.clients:
-            if game.get_uuid() == i.game.get_uuid():
-                write_package(i.socket, PKG_GAME_OVER, {'winner': game.get_scores()[0][0]})
-                if game.get_uuid() in self.ServerGames:
-                    del self.ServerGames[game.get_uuid()]  
+
+        for i in self.game.users.keys():
+            client_callbacks[i].gameOver(game.get_scores()[0][0])
+        self.ServerGames.remove(game.get_uuid())
         print "Game over, game uuid: %s" % game.get_uuid()
-        
-            
+
+
     def remove_client(self, username):
         """Remove client from game"""
-        for i in range(0, len(self.clients)):
-            if self.clients[i].username == username:
-                del self.clients[i]
-                self.ServerUsernames.remove(username)
+        self.client_callbacks.remove(username)
+        self.ServerUsernames.remove(username)
+
     def start_game(self, game):
         """Send the start packet, used when game is full of players"""
-        for i in self.clients:
-            if game.get_uuid() == i.game.get_uuid():
-                write_package(i.socket, PKG_SESSION_STARTED, {})
-                write_package(i.socket, PKG_SUDOKU_STATE, \
-                        {'sudoku': game.get_sudoku().serialize()})
+        for i in self.game.users.keys():
+            client_callbacks[i].sessionStarted()
         print "Game starting, game uuid: %s" % game.get_uuid()
-             
+
+
 class Game():
     """Represents the sudoku game on the server side.
     Creates a sudoku object and keeps track of points"""
-    
+
     def __init__(self, name, num_players, username):
         """Game constructor, initialize Sudoku game and its needed variables"""
         self.__num_players = num_players
         self.__uuid = str(uuid.uuid4())
         self.__sudoku = Sudoku()
-        self.__users = {username : 0} # username: score
+        self.users = {username : 0} # username: score
         self.__game_name = name
         print "New game '%s' created by user '%s', with max %d users and uuid %s" % \
               (name, username, num_players, self.__uuid)
-    
+
     def join(self, username):
         """Used to make a new user join the game"""
         assert(not self.is_full())
-        self.__users[username] = 0
+        self.users[username] = 0
 
     def get_uuid(self):
         """Returns the unique identifier of the game"""
@@ -98,22 +80,22 @@ class Game():
         return self.__sudoku
 
     def get_scores(self):
-        """Returns an array tuples of the users with their score. 
+        """Returns an array tuples of the users with their score.
         Sorted from winner to loser (big to small score)"""
-        return sorted(self.__users.items(), key = lambda i: i[1]) # convert dict to array of tuples
+        return sorted(self.users.items(), key = lambda i: i[1]) # convert dict to array of tuples
 
     def get_num_players(self):
         """Returns the maximal number of players in this game"""
-        return self.__num_players    
+        return self.__num_players
 
     def get_cur_num_players(self):
         """Returns the current number of players in this game"""
-        return len(self.__users)
+        return len(self.users)
 
     def is_full(self):
         """Returns true if current number of players == maximal number of players"""
-        return len(self.__users) == self.__num_players
-        
+        return len(self.users) == self.__num_players
+
     def insert_number(self, username, i, j, number):
         """Used to insert a number in the Sudoku grid, returns:
              1  if valid
@@ -121,145 +103,100 @@ class Game():
             -1  if not valid
         Updates the users points and also returns True if game is finished"""
         point, finish = self.__sudoku.insert(i, j, number)
-        self.__users[username] += point
+        self.users[username] += point
         return point, finish
-        
+
     def leave_game(self, username):
         """Used to leave the game, returns True if only 1 player left"""
-        del self.__users[username]
-        if len(self.__users) < 1:
+        del self.users[username]
+        if len(self.users) < 1:
             del manager.ServerGames[self.get_uuid()]
         return self.get_cur_num_players() == 1
-        
+
     def get_game_name(self):
-        """Returns the given name for a game"""  
+        """Returns the given name for a game"""
         return self.__game_name
 
-class ClientThread(threading.Thread):
-    """Thread object, one for each new client. Used to respond to clients requests"""
-    
-    def __init__(self, client_socket, client_address, manager):
-        """Constructor. Keep information about the client
-        like username and which game he's in"""
-        
-        threading.Thread.__init__(self)
+
+class SudokuServer(object):
+    def __init__(self):
         self.username = None
-        self.socket = client_socket
-        self.socket.setblocking(False)
         self.address = client_address
         self.game = None
-        self.should_stop = False
-    
-    def run(self): 
-        """Dispatch cases according to packet type and according to protocol.txt"""
-        
-        finish = False 
-        try:
-            stream = util.SocketWrapper(self.socket)
-            while not self.should_stop:
-                stream.receive()
-                if not stream.available():
-                    time.sleep(0.01)
-                    continue
-                
-                # Read incoming packet
-                pkg_type, data = read_package(stream)
-                
-                # Connection
-                if pkg_type == PKG_HELLO:
-                    if data['username'] not in manager.ServerUsernames:
-                        print "Received PKG_HELLO"
-                        manager.ServerUsernames.append(data['username'])
-                        self.username = data['username']
-                        write_package(self.socket, PKG_HELLO_ACK, {'ok' : True})
-                    else:
-                        write_package(self.socket, PKG_HELLO_ACK, {'ok' : False})
-                
-                # Get list of sessions
-                elif pkg_type == PKG_GET_SESSIONS:
-                    print "Received PKG_GET_SESSIONS"
-                    out = {'sessions': []}
-                    for i in manager.ServerGames.keys():
-                        self.game = manager.ServerGames[i]
-                        out['sessions'].append( \
-                        (i, self.game.get_game_name(), self.game.get_cur_num_players(),\
-                         self.game.get_num_players())) 
-                    write_package(self.socket, PKG_SESSIONS, out)
-                
-                # Join existing session   
-                elif pkg_type == PKG_JOIN_SESSION:
-                    print "Received PKG_JOIN_SESSION"
-                    if data['uuid'] not in manager.ServerGames \
-                    or manager.ServerGames[data['uuid']].is_full():
-                        write_package(self.socket, PKG_SESSION_JOINED, \
-                                      {'ok' : False, 'uuid' : data['uuid']})
-                    else:
-                        self.game = manager.ServerGames[data['uuid']]
-                        self.game.join(self.username)
-                        write_package(self.socket, PKG_SESSION_JOINED, \
-                                      {'ok' : True, 'uuid' : data['uuid']}) 
-                        
-                        if self.game.is_full():
-                            manager.start_game(self.game) 
-                                      
-                        #write_package(self.socket, PKG_SUDOKU_STATE, \
-                        #              {'sudoku': self.game.get_sudoku().serialize()})
-                        write_package(self.socket, PKG_SCORES_STATE, {'scores': self.game.get_scores()})      
-                        manager.notify_score(self.game)      
-                        
-                # Create new session   
-                elif pkg_type == PKG_CREATE_SESSION:
-                    print "Received PKG_CREATE_SESSION"
-                    self.game = Game(data['name'], data['num_players'], self.username)
-                    manager.ServerGames[self.game.get_uuid()] = self.game
-                    write_package(self.socket, PKG_SESSION_JOINED, \
-                                  {'ok' : True, 'uuid' : self.game.get_uuid()})
-                    if self.game.is_full():
-                        manager.start_game(self.game)                       
-                    #write_package(self.socket, PKG_SUDOKU_STATE, \
-                    #              {'sudoku': self.game.get_sudoku().serialize()})
-                    write_package(self.socket, PKG_SCORES_STATE, {'scores': self.game.get_scores()})
-                    
-                    
-                # Player suggest a number
-                elif pkg_type == PKG_SUGGEST_NUMBER:
 
-                    print "Received PKG_SUGGEST_NUMBER"
-                    point, finish = self.game.insert_number(self.username, data['i'], \
-                                                            data['j'], data['number'])
-                    write_package(self.socket, PKG_SUGGEST_NUMBER_ACK, \
-                                  {'ok' : point > 0, 'i' : data['i'], 'j' : data['j']})
-                    
-                    # Notify to everyone in the same game
-                    manager.notify(self.game)
-                    print "%s wrote %d in position (%d, %d) in the Sudoku with game uuid %s" % \
-                          (self.username, data['number'], data['i'], data['j'], self.game.get_uuid())
-                    if finish:
-                        manager.game_over(self.game)      
-                # Player wants to leave
-                elif pkg_type == PKG_LEAVE_SESSION:
-                    print "Received PKG_LEAVE_SESSION"
-                    finish = self.game.leave_game(self.username)
+    @Pyro4.expose
+    def listSessions(self):
+        for i in manager.ServerGames.keys():
+            self.game = manager.ServerGames[i]
+            result.append( \
+            (i, self.game.get_game_name(), self.game.get_cur_num_players(),\
+             self.game.get_num_players()))
+        return result
 
-                    if finish:
-                        manager.game_over(self.game)
-            
-            self.socket.shutdown(SHUT_WR)
-            self.socket.close()
-        except Exception, msg:
-            traceback.print_exc()
-            self.socket.close()
-            manager.remove_client(self.username)
-            print "Client disconnected"
-    
-    def stop(self):
-        """Check if thread has to stop"""
-        self.should_stop = True
+    @Pyro4.expose
+    def createSession(self, name, num_players):
+        self.game = Game(name, num_players, self.username)
+        manager.ServerGames[self.game.get_uuid()] = self.game
+        if self.game.is_full():
+            manager.start_game(self.game)
+        return True
+
+    @Pyro4.expose
+    def joinSession(self, session):
+        if session not in manager.ServerGames \
+        or session.is_full():
+            return False
+        else:
+            self.game = session
+            self.game.join(self.username)
+            return True
+
+            if self.game.is_full():
+                manager.start_game(self.game)
+
+            manager.notify_score(self.game)
+
+    @Pyro4.expose
+    def provideSessionCallback(self, callback):
+        # check if he's really in session... blah
+        # register callback for server
+        # it can be used later to notify the client about session changes
+        # see client.py SessionHandler
+
+        # smth like
+        # self.client_callbacks[get_id()] = callback
+
+        print("Got client callback: %s" % callback)
+        manager.client_callbacks[self.username] = callback
+        def answer(callback=callback):
+            callback.sessionStarted()
+            time.sleep(5)
+            callback.gameOver("You!")
+        threading.Thread(target=answer).start()
+
+    @Pyro4.expose
+    def suggestNumber(self, i, j, value):
+        point, finish = self.game.insert_number(self.username, i, j, value)
+
+        # Notify to everyone in the same game
+        manager.notify(self.game)
+        print "%s wrote %d in position (%d, %d) in the Sudoku with game uuid %s" % \
+              (self.username, value, i, j, self.game.get_uuid())
+        if finish:
+            manager.game_over(self.game)
+        return point
+
+    @Pyro4.expose
+    def leaveSession(self):
+        finish = self.game.leave_game(self.username)
+        if finish:
+            manager.game_over(self.game)
+        return
 
 
 if __name__ == '__main__':
     """Server entry point"""
-    
+
     # Parse args
     parser = ArgumentParser(description="Concurrent Sudoku", version = "1.0")
     parser.add_argument('-H','--host', help='Server TCP port, '\
@@ -268,41 +205,10 @@ if __name__ == '__main__':
     parser.add_argument('-p','--port', type=int, help='Server TCP port, '\
                         'defaults to %d' % SERVER_PORT, default=SERVER_PORT)
     args = parser.parse_args()
-    
-    # Create Manager 
-    manager = Manager()
-    
-    # Create and bind to server socket to listen to requests
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind((args.host, args.port))
 
-    backlog = 0 
-    s.listen(backlog)
-       
-    # Receive loop
-    while True:
-        try:
-            print "Listening..."
-            client_socket, client_address = s.accept()
-            client_thread = ClientThread(client_socket, client_address, manager)
-            client_thread.start()
-            print "New client connected"
-            manager.clients.append(client_thread)
-        
-        except KeyboardInterrupt as e:
-            print 'Ctrl+C issued ...'
-            print 'Terminating server ...'
-            break
-        except Exception, msg:
-            print 'Exception %s' % msg
-    
-    # Join all client threads       
-    for i in manager.clients:
-        i.stop()
-        i.join()        
-        print "The client %s closed its connection" % str(client_address)
-    
-    # Close server socket
-    s.close()
-    print 'Closed the server socket'
-    print 'Terminating ...'
+    # Create Manager
+    manager = Manager()
+
+    Pyro4.Daemon.serveSimple({
+        SudokuServer: "sudoku"
+    }, host=args.host, port=args.port, ns=False)
